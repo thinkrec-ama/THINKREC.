ddd94ffb69f94e38872edd238aaafd94f174f8c7
yama <devthinkrec@gmail.com>
2022-01-27 14:42:18 +0900

    [update] コミットしていなかった分をコミット

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,24 @@
 var THINKREC = THINKREC || {};
 
+THINKREC.NAV_CONTROLLER = {
+    init: function () {
+        this.setParameters();
+        this.bindEvent();
+    },
+    setParameters: function () {
+        this.navToggle = $(".jcsMenuToggle");
+        this.menuLine = $(".jcsMenuLine");
+        this.gNav = $(".jscGnav");
+    },
+    bindEvent: function () {
+        this.navToggle.on("click", $.proxy(this.navMove, this));
+    },
+    navMove: function () {
+        this.menuLine.toggleClass('active');
+        this.gNav.fadeToggle();
+    }
+};
+
 THINKREC.HEADER_DISPLAY = {
     init: function () {
         this.setParameters();
@@ -53,8 +72,58 @@ THINKREC.PAGETOP_CONTROLLER = {
         this.$body.animate({ scrollTop: this.TOP_OFFSET }, this.ANIMATION_TIME);
     },
 };
+function sliderStart() {
+
+    const slide = document.getElementById('kvSliderList');      //スライダー親
+    const slideItem = slide.querySelectorAll('.slide_item');   //スライド要素
+    const totalNum = slideItem.length - 1;                     // スライドの枚数を取得
+    const FadeTime = 2000;                                     //フェードインの時間
+    const IntarvalTime = 5000;                                 //クロスフェードさせるまでの間隔
+    let actNum = 0;                                            //現在アクティブな番号
+    let nowSlide;                                              //現在表示中のスライド
+    let NextSlide;                                             //次に表示するスライド
+
+    // DOM読み込み時にスライドの1枚目をフェードイン
+    slideItem[0].classList.add('show_', 'zoom_');
+
+    // 処理を繰り返す
+    setInterval(() => {
+        if (actNum < totalNum) {
+
+            nowSlide = slideItem[actNum];
+            NextSlide = slideItem[++actNum];
+
+            //.show_削除でフェードアウト
+            nowSlide.classList.remove('show_');
+            // と同時に、次のスライドがズームしながらフェードインする
+            NextSlide.classList.add('show_', 'zoom_');
+            //フェードアウト完了後、.zoom_削除
+            setTimeout(() => {
+                nowSlide.classList.remove('zoom_');
+            }, FadeTime);
+
+
+        } else {
+
+            nowSlide = slideItem[actNum];
+            NextSlide = slideItem[actNum = 0];
+
+            //.show_削除でフェードアウト
+            nowSlide.classList.remove('show_');
+            // と同時に、次のスライドがズームしながらフェードインする
+            NextSlide.classList.add('show_', 'zoom_');
+            //フェードアウト完了後、.zoom_削除
+            setTimeout(() => {
+                nowSlide.classList.remove('zoom_');
+            }, FadeTime);
+
+        };
+    }, IntarvalTime);
+
+}
 
 $(window).on("load", function () {
+    THINKREC.NAV_CONTROLLER.init();
     THINKREC.HEADER_DISPLAY.init();
     THINKREC.PAGETOP_CONTROLLER.init();
     $(".loadingWrap").delay(1500).fadeOut('slow');
